3765c7f9db2244a685184eef7406e05447e24cdc
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2020-12-04 14:53:36 +0200

    Update and rename 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/Reconnaissance/main.cpp to 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/32A - Reconnaissance.cpp

---
+++ 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/32A - Reconnaissance.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pi               acos(-1)
+#define inc(a)           a.begin(), a.end()
+#define dec(a)           a.rbegin(), a.rend()
+#define Unique(n)        (n).erase(unique(inc(n)), (n).end())
+#define what_is(x)       cerr << #x << " is " << x << '\n';
+#define to(i, a, n)      for(int i=a; i<n; i++)
+#define fr(i, a, n)      for(int i=a; i>=n; i--)
+#define read(arr, l, r)  for(int i=l; i<r; cin >> arr[i++])
+#define prnt(arr, l, r)  for(int i=l; i<r; cout << arr[i++] << " \n"[i==r])
+
+typedef long long ll;
+typedef long double ld;
+
+int dx[] = {0, 1,  0, -1};
+int dy[] = {1, 0, -1,  0};
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m, c=0;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    read(a, 0, n);
+
+    to(i, 0, n) to(j, i+1, n)
+        if(abs(a[j]-a[i])<=m) c++;
+
+    cout << c*2;
+    return 0;
+}
