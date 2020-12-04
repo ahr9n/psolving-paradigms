9b0fa9b774f74ef5369fef581249049e21646ffd
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2020-12-04 14:38:02 +0200

    Update and rename 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/Autocomplete/Autocomplete.cbp to 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/53A - Autocomplete.cpp

---
+++ 1.Brute-Force/1.1.Iterative-Brute-Force/Codeforces-problems/53A - Autocomplete.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pi               acos(-1)
+#define inc(a)           a.begin(), a.end()
+#define dec(a)           a.rbegin(), a.rend()
+#define Unique(n)        (n).erase(unique(inc(n)), (n).end())
+#define what_is(x)       cerr << #x << " is " << x << '\n';
+#define to(i, a, n)      for(int i=a; i<n; i++)
+#define fr(i, a, n)      for(int i=a; i>=n; i--)
+#define read(arr, l, r)  for(int i=l; i<r; cin >> arr[i++])
+#define prnt(arr, l, r)  for(int i=l; i<r; cout << arr[i++] << " \n"[i==r])
+
+typedef long long ll;
+typedef long double ld;
+
+int dx[] = {0, 1,  0, -1};
+int dy[] = {1, 0, -1,  0};
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s, t;
+    int n; cin >> s >> n;
+    set<string> st;
+    while(n--){
+        cin >> t;
+        if(t.substr(0, min(s.size(), t.size())) == s) st.insert(t);
+    }
+    cout << (st.empty() ? s : *st.begin());
+
+    return 0;
+}
