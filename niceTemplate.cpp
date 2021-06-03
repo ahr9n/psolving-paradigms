e57b7ee27100e6ab05efc1f551be47e9d053a078
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2021-06-03 18:25:24 +0200

    Update niceTemplate.cpp

--- niceTemplate.cpp
+++ niceTemplate.cpp
@@ -1,28 +1,29 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("-O2")
 
 using namespace std;
+using ll = long long;
+using ld = long double;
 
-#define pi               acos(-1)
-#define inc(a)           a.begin(), a.end()
-#define dec(a)           a.rbegin(), a.rend()
-#define Unique(n)        (n).erase(unique(inc(n)), (n).end())
-#define what_is(x)       cerr << #x << " is " << x << '\n';
-#define to(i, a, n)      for(int i=a; i<n; i++)
-#define fr(i, a, n)      for(int i=a; i>=n; i--)
-#define read(arr, l, r)  for(int i=l; i<r; cin >> arr[i++])
-#define prnt(arr, l, r)  for(int i=l; i<r; cout << arr[i++] << " \n"[i==r])
+const int N = 2E5 + 5, M = 2E6 + 10, MOD = 1E9 + 7, OO = 0x3f3f3f3f;;
+int t, n, a[N];
 
-typedef long long ll;
-typedef long double ld;
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+// #endif /*ONLINE_JUDGE*/
 
-int dx[] = {0, 1,  0, -1};
-int dy[] = {1, 0, -1,  0};
+//     cin >> t;
+//     while (t--) {
+//         cin >> n;
+//         for (int i = 0; i < n; ++i) cin >> a[i];
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
 
-    // code here :D
+//     }
 
     return 0;
 }
