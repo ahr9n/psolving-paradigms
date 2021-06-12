a9b3ae53df5b4595d75c588cda8b568c87aaf01c
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2021-06-12 18:20:49 +0200

    Create CodeForces 26B - Regular Bracket Sequence.cpp

---
+++ 2.Greedy/CodeForces 26B - Regular Bracket Sequence.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pi               acos(-1)
+#define inc(a)           a.begin(), a.end()
+#define dec(a)           a.rbegin(), a.rend()
+#define Unique(n)        (n).erase(unique(inc(n)), (n).end())
+#define what_is(x)       cerr << #x << " is " << x << '\n';
+#define to(i, a, n)      for(int i=a; i<n; i++)
+#define fr(i, a, n)      for(int i=a; i>=n; i--)
+#define read(arr, l, r)  for(int i=l; i<r; cin >> arr[i++])
+#define prnt(arr, l, r)  for(int i=l; i<r; cout << arr[i++] << " \n"[i==r])
+
+typedef long long ll;
+typedef long double ld;
+
+int dx[] = {0, 1,  0, -1};
+int dy[] = {1, 0, -1,  0};
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 0;
+    string s; cin >> s;
+    
+    stack<char> st;
+    for(auto &it:s)
+        if(it == ')' && !st.empty() && st.top() == '(') 
+            st.pop(), n += 2;
+        else 
+            st.push(it);
+
+    cout << n;
+
+    return 0;
+}
