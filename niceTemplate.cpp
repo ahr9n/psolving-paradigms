00d10a592d159ed795dfdb778e49684e263a70b6
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2021-08-07 06:26:45 +0200

    Update niceTemplate.cpp

--- niceTemplate.cpp
+++ niceTemplate.cpp
@@ -7,23 +7,17 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-const int N = 2E5 + 5, M = 2E6 + 10, MOD = 1E9 + 7, OO = 0x3f3f3f3f;;
-int t, n, a[N];
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+const int N = 2E5 + 5, M = 2E6 + 10, MOD = 1E9 + 7, OO = 0x3f3f3f3f;
 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0);
-    
-// #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-// #endif /*ONLINE_JUDGE*/
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif /*ONLINE_JUDGE*/
 
-//     cin >> t;
-//     while (t--) {
-//         cin >> n;
-//         for (int i = 0; i < n; ++i) cin >> a[i];
-
-
-//     }
 
     return 0;
 }
